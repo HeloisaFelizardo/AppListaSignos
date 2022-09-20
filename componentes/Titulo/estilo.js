db93e4e982ca52f1aea3848487d50c7e8ed9315e
Heloisa Felizardo <helocmp@yahoo.com.br>
2022-09-20 19:41:29 -0300

    First commit

---
+++ componentes/Titulo/estilo.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const estilo = StyleSheet.create({
+  box: {
+    backgroundColor: '#000',
+    alignItems: 'center',
+    paddingTop: 50,
+    paddingBottom: 30,
+  },
+  texto:{
+    fontSize: 28,
+    color: '#61dafb'
+  }
+});
+
+export default estilo;
